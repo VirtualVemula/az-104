c0993b7b17546370fce7077d96c9b6d5f6e32826
virtualvemula <vijay@virtualvemula.com>
2021-08-08 11:58:14 +0530

    updated the script with User creation commands

--- 01-Create AAD Users Using PS.ps1
+++ 01-Create AAD Users Using PS.ps1
@@ -1,8 +1,10 @@
-#Create AAD USers
 #Creation of AAD Users using Powershell
 #Install Azure AD Powershell Modules.
 Install-Module -Name AzureAD
 #Connect to Azure AD
 $AzureADCred = Get-Credential
 Connect-Azuread -Credential $AzureADCredconn
-
+#Create user vuser4 using Powershell, replace -UserPrinicipalName your default domain name.
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PasswordProfile.Password = "Test@123"
+New-AzureADUser -AccountEnabled $True -DisplayName "Vuser4" -PasswordProfile $PasswordProfile -MailNickName "Vuser4" -UserPrincipalName "Vuser4@vemula4uhotmail.onmicrosoft.com"
