4e4ae3daed2799b2a84ae4d70a20f8263d37501c
virtualvemula <vijay@virtualvemula.com>
2021-08-08 11:43:44 +0530

    updated

--- Create AAD Users Using PS.ps1
+++ Create AAD Users Using PS.ps1
@@ -1,2 +1,8 @@
 #Create AAD USers
-# Creation of AAD Users using Powershell
+#Creation of AAD Users using Powershell
+#Install Azure AD Powershell Modules.
+Install-Module -Name AzureAD
+#Connect to Azure AD
+$AzureADCred = Get-Credential
+Connect-Azuread -Credential $AzureADCredconn
+
